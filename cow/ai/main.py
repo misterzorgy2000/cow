ed99e08e186726dcf93dfb5a51053e4b3ce8593a
misterzorgy <misterzorgy2000@yandex.ru>
2024-08-25 17:39:25 +0300

    update

--- cow/ai/main.py
+++ cow/ai/main.py
@@ -1,9 +1,5 @@
 from colorama import init
 
-import pprint
-
-from cow.ai.scripts.data import save_metrics
-from cow.ai.scripts.fit import fit_model
 import oslo_messaging
 from oslo_messaging import get_notification_transport
 from oslo_messaging import serializer as oslo_serializer
@@ -20,8 +16,14 @@ NOTICE_MODEL_CREATED = {
 init(autoreset=True)
 
 def run_ai():    
-    save_metrics()
-    fit_model()    
+    transport = get_notification_transport(cfg.CONF, 'rabbit://stackrabbit:secret@89.169.168.186:5672/')
+    notifier = oslo_messaging.Notifier(transport=transport, serializer=oslo_serializer.JsonPayloadSerializer(), topics=['notifications'])
+    notifier.prepare('compute.vagrant-precise')
+    try: 
+        notifier.info({}, 'compute.instance.create.end', NOTICE_MODEL_CREATED)
+        print('ok')
+    except Exception:
+        print(Exception)
         
 if __name__ == '__main__':
     run_ai()
